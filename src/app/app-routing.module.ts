a7df02209f17c80c41fc1486b331fca0b0cc07a3
oaymohsin <oaymohsin@gmail.com>
2022-08-17 14:18:23 +0500

    push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './MainPortal/ContactUs/contact-us/contact-us.component';
+import { HomeComponent } from './MainPortal/Home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'contactus',
+    component:ContactUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './MainPortal/Header/header/header.component';
+import { FooterComponent } from './MainPortal/Footer/footer/footer.component';
+import { HomeComponent } from './MainPortal/Home/home/home.component';
+import { ContactUsComponent } from './MainPortal/ContactUs/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
